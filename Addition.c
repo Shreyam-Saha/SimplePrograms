caa4d2466a7d7ce5463a0c8746f00639d74c1d52
Programmer <55909772+YashMirge@users.noreply.github.com>
2020-10-01 00:02:19 +0530

    Updated

--- Addition.c
+++ Addition.c
@@ -3,9 +3,9 @@ int main()
 {
     int a,b,c;
     printf(Enter the number:);
-    scanf("%d",a);
+    scanf("%d",&a);
     printf(Enter the 2nd number:);
-    scanf("%d",b);
+    scanf("%d",&b);
     c=a+b;
     printf(The sum is "%d",c);
 }
